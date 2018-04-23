a5265d348875bdbd69a40136315d1e0c918a552d
Santiago Barbat <santiago@winebuyers.com>
2018-04-23 11:49:54 +0100

    Configuration.php file

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Winebuyers\SyliusSendgridPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sylius_mailer');
+
+        return $treeBuilder;
+    }
+}
